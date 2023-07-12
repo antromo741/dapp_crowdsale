d8d38f1e663fc2c2dd9b5d9b9a0e65cb7ab70e71
antromo741 <anthonyroma741@gmail.com>
2023-07-12 13:08:15 -0400

    add spinner

--- src/components/Buy.js
+++ src/components/Buy.js
@@ -1,28 +1,35 @@
-import React, { useState } from 'react'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Spinner from 'react-bootstrap/Spinner'
-import { ethers } from 'hardhat'
+import { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Spinner from 'react-bootstrap/Spinner';
+import { ethers } from 'ethers'
 
 const Buy = ({ provider, price, crowdsale, setIsLoading }) => {
     const [amount, setAmount] = useState('0')
+    const [isWaiting, setIsWaiting] = useState(false)
 
     const buyHandler = async (e) => {
         e.preventDefault()
-        console.log('buying tokens', amount)
+        setIsWaiting(true)
 
-        const signer = await provider.getSigner()
+        try {
+            const signer = await provider.getSigner()
 
-        const value = ethers.utils.parseUnits((amount * price).toString(), 'ether')
+            // We need to calculate the required ETH in order to buy the tokens...
+            const value = ethers.utils.parseUnits((amount * price).toString(), 'ether')
+            const formattedAmount = ethers.utils.parseUnits(amount.toString(), 'ether')
 
-        const formattedAmount = ethers.utils.parseUnits(amount.toString(), 'ether')
+            const transaction = await crowdsale.connect(signer).buyTokens(formattedAmount, { value: value })
+            await transaction.wait()
+        } catch {
+            window.alert('User rejected or transaction reverted')
+        }
 
-        const transaction = await crowdsale.connect(signer).buyTokens(formattedAmount, { value: value })
-
-        await transaction.wait()
+        setIsLoading(true)
     }
+
     return (
         <Form onSubmit={buyHandler} style={{ maxWidth: '800px', margin: '50px auto' }}>
             <Form.Group as={Row}>
@@ -30,14 +37,17 @@ const Buy = ({ provider, price, crowdsale, setIsLoading }) => {
                     <Form.Control type="number" placeholder="Enter amount" onChange={(e) => setAmount(e.target.value)} />
                 </Col>
                 <Col className='text-center'>
-                    <Button variant="primary" type="submit" style={{ width: "100%" }}>
-                        Buy Tokens
-                    </Button>
+                    {isWaiting ? (
+                        <Spinner animation="border" />
+                    ) : (
+                        <Button variant="primary" type="submit" style={{ width: '100%' }}>
+                            Buy Tokens
+                        </Button>
+                    )}
                 </Col>
             </Form.Group>
-
         </Form>
-    )
+    );
 }
 
-export default Buy
+export default Buy;
